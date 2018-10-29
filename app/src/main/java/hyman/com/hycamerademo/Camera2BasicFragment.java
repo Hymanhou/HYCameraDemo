3c7cdd1cf0d33da14d6c26d291b75118ecf02d5d
hyuan <hyuanhit@126.com>
2018-10-29 23:30:47 +0800

    COMMIT

--- app/src/main/java/hyman/com/hycamerademo/Camera2BasicFragment.java
+++ app/src/main/java/hyman/com/hycamerademo/Camera2BasicFragment.java
@@ -3,12 +3,32 @@ package hyman.com.hycamerademo;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.util.SparseIntArray;
+import android.view.Surface;
 import android.view.View;
 
 public class Camera2BasicFragment extends Fragment implements View.OnClickListener,
         ActivityCompat.OnRequestPermissionsResultCallback {
 
     private static final SparseIntArray ORIENTATIONS = new SparseIntArray();
+    private static final int REQUEST_CAMERA_PERMISSION = 1;
+    private static final String FRAGMENT_DIALOG = "dialog";
+
+    static {
+        ORIENTATIONS.append(Surface.ROTATION_0, 90);
+        ORIENTATIONS.append(Surface.ROTATION_90, 0);
+        ORIENTATIONS.append(Surface.ROTATION_180, 270);
+        ORIENTATIONS.append(Surface.ROTATION_270, 180);
+    }
+
+    private static final String TAG = "Camera2BasicFragment";
+    private static final int STATE_PREVIEW = 0;
+    private static final int STATE_WAITING_LOCK = 1;
+    private static final int STATE_WAITING_PRECAPTURE = 2;
+    private static final int STATE_WAITING_NON_PRECAPTURE = 3;
+    private static final int STATE_PICTURE_TAKEN = 4;
+
+    private static final int MAX_PREVIEW_WIDTH = 1920;
+    private static final int MAX_PREVIEW_HEIGHT = 1080;
 
     @Override
     public void onClick(View v) {
