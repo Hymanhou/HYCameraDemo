2e9230eea78449e13435b7cb6d603b6d0838ad9d
houyuan <houyuan@weiyankeji.cn>
2018-10-29 20:18:27 +0800

    commit code

---
+++ app/src/main/java/hyman/com/hycamerademo/AudoFitTextureView.java
@@ -0,0 +1,48 @@
+package hyman.com.hycamerademo;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.TextureView;
+
+public class AudoFitTextureView extends TextureView {
+
+    private int mRatioWidth = 0;
+    private int mRatioHeight = 0;
+
+    public AudoFitTextureView(Context context) {
+        super(context);
+    }
+
+    public AudoFitTextureView(Context context, AttributeSet attrs) {
+        super(context, attrs, 0);
+    }
+
+    public AudoFitTextureView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public void setAspectRatio(int width, int height) {
+        if (width < 0 || height < 0) {
+            throw new IllegalArgumentException("size cann't be negative.");
+        }
+        mRatioWidth = width;
+        mRatioHeight = height;
+        requestLayout();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int width = MeasureSpec.getSize(widthMeasureSpec);
+        int height = MeasureSpec.getSize(heightMeasureSpec);
+        if (0 == mRatioWidth || 0 == mRatioHeight) {
+            setMeasuredDimension(width, height);
+        } else {
+            if (width < height * mRatioWidth / mRatioHeight) {
+                setMeasuredDimension(width, width * mRatioHeight / mRatioWidth);
+            } else {
+                setMeasuredDimension(height * mRatioWidth / mRatioHeight, height);
+            }
+        }
+    }
+}
---
+++ app/src/main/java/hyman/com/hycamerademo/Camera2BasicFragment.java
@@ -0,0 +1,17 @@
+package hyman.com.hycamerademo;
+
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
+import android.util.SparseIntArray;
+import android.view.View;
+
+public class Camera2BasicFragment extends Fragment implements View.OnClickListener,
+        ActivityCompat.OnRequestPermissionsResultCallback {
+
+    private static final SparseIntArray ORIENTATIONS = new SparseIntArray();
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
